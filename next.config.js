11f38b7a310be67a6213e52a1e5cbcf1c9d8c691
Adriel Baez <baez.adriel27@gmail.com>
2022-05-24 16:14:12 -0300

    [add] new sites to get images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["images6.alphacoders.com", "images.pexels.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

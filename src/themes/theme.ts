997008b2e9eaf8ea2efdf0b6f0bb77cf4773d70c
Adriel Baez <baez.adriel27@gmail.com>
2022-05-24 16:23:45 -0300

    [add] new changes in the theme.ts

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -3,11 +3,14 @@ import { extendTheme } from "@chakra-ui/react";
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-    400: "blue",
+  primary: {
+    900: "#030334",
+    800: "#F0F0FE",
+    400: "#5353C6",
+  },
+  secondary: {
+    400: "#5F5F5F",
+    500: "#8D9199",
   },
 };
 
